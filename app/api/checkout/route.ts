9bd5895cd018d36f6cf73336c71f2f8da1028927
njauvictor <141951816+njauvictor@users.noreply.github.com>
2024-05-10 18:43:38 +0300

    cc

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { stripe } from "@/lib/stripe";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
@@ -18,11 +19,37 @@ export async function POST(req: NextRequest) {
       return new NextResponse("Not enough data to checkout", { status: 400 });
     }
 
-    // Instead of processing payment, send order details to WhatsApp
-    // Replace the placeholder with your actual implementation to send order details to WhatsApp
-    const whatsappURL = `https://wa.me/${process.env.SELLER_PHONE_NUMBER}?text=${encodeURIComponent(JSON.stringify({ cartItems, customer }))}`;
-    
-    return NextResponse.redirect(whatsappURL); // Redirect to WhatsApp URL
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
+      },
+      shipping_options: [
+        { shipping_rate: "shr_1PEfC4D0qxGKHmyEoe159VJB" },
+        
+      ],
+      line_items: cartItems.map((cartItem: any) => ({
+        price_data: {
+          currency: "cad",
+          product_data: {
+            name: cartItem.item.title,
+            metadata: {
+              productId: cartItem.item._id,
+              ...(cartItem.size && { size: cartItem.size }),
+              ...(cartItem.color && { color: cartItem.color }),
+            },
+          },
+          unit_amount: cartItem.item.price * 100,
+        },
+        quantity: cartItem.quantity,
+      })),
+      client_reference_id: customer.clerkId,
+      success_url: `${process.env.ECOMMERCE_STORE_URL}/payment_success`,
+      cancel_url: `${process.env.ECOMMERCE_STORE_URL}/cart`,
+    });
+
+    return NextResponse.json(session, { headers: corsHeaders });
   } catch (err) {
     console.log("[checkout_POST]", err);
     return new NextResponse("Internal Server Error", { status: 500 });
--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -19,29 +19,24 @@ const LeftSideBar = () => {
   }
 
   return (
-    <div className="h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-900 shadow-xl max-lg:hidden">
-      <Image src="/Online Duka (2).png" alt="logo" width={150} height={70} />
-
-      <div className="flex flex-col gap-12">
-        {navLinks.map((link) => (
-          <Link
-            href={link.url}
-            key={link.label}
-            className={`flex gap-4 text-body-medium hover:text-blue-200 active:text-blue-300 ${
-              pathname === link.url ? "text-blue-200" : "text-white"
-            }`}
-            onClick={handleClick}
-          >
-            {link.icon} <p>{link.label}</p>
-          </Link>
-        ))}
-      </div>
-
+    <div className="min-h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-900 shadow-xl max-lg:hidden">
+      <Image src="/onlineduka.png" alt="logo" width={120} height={80} className='max-lg:hidden'/>
+      {navLinks.map((link) => (
+        <Link
+          href={link.url}
+          key={link.label}
+          className={`flex gap-4 text-body-medium hover:text-blue-200 active:text-blue-300 ${
+            pathname === link.url ? "text-blue-200" : "text-white"
+          }`}
+          onClick={handleClick}
+        >
+          {link.icon} <p>{link.label}</p>
+        </Link>
+      ))}
       <div className="flex gap-4 text-white text-body-medium items-center">
         <UserButton />
         <p>Edit Profile</p>
       </div>
-
       {isLoading && <div>Loading...</div>}
     </div>
   );
